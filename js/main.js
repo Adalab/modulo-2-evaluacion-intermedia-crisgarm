b27135a082cac1f4b1d88ecaf103ad5689002915
crisgarm <crisgarm9@gmail.com>
2020-10-13 09:39:02 +0200

    Declare variables and listener

--- js/main.js
+++ js/main.js
@@ -6,4 +6,15 @@ function getRandomNumber(max) {
 
  console.log("Mi número aleatorio es " + getRandomNumber(100));
 
- 
+const input = document.querySelector(".js-input");
+const btn = document.querySelector(".js-button");
+const clues = document.querySelector(".js-clues");
+const attempts = document.querySelector(".js-attempts");
+// const random = getRandomNumber();
+
+function updateClick(){
+}
+
+
+
+ btn.addEventListener("click", updateClick);
