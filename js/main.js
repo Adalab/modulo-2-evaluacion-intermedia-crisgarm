c63b67c710e8bac350e1b58df4b0bcb648ee6d33
crisgarm <crisgarm9@gmail.com>
2020-10-13 10:52:06 +0200

    Add conditionals

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
 "use strict";
 
 function getRandomNumber(max) {
-  return Math.ceil(Math.random() * max);
+  const random = Math.ceil(Math.random() * max);
+  return random;
  }
-
- console.log("Mi número aleatorio es " + getRandomNumber(100));
 
 const input = document.querySelector(".js-input");
 const btn = document.querySelector(".js-button");
 const clues = document.querySelector(".js-clues");
 const attempts = document.querySelector(".js-attempts");
+const random = getRandomNumber(100);
+console.log("Mi número aleatorio es " + random);
 
 function updateClick(){
   const inputValue = input.value;
   input.innerHTML = inputValue;
   console.log(inputValue);
-
+  console.log(random);
+  if (inputValue < 1 || inputValue > 100){
+    clues.innerHTML = "El número debe estar entre 1 y 100."
+    console.log(clues);
+   }else if (inputValue == random){
+    clues.innerHTML = "¡Has ganado campeona!"
+    console.log(clues);
+   }else if(inputValue > random){
+    clues.innerHTML = "Pista: Demasiado alto."
+    console.log(clues);
+    }else{
+    clues.innerHTML = "Pista: Demasiado bajo."
+    console.log(clues);
+}
 }
 
-
-
- btn.addEventListener("click", updateClick);
+btn.addEventListener("click", updateClick);
