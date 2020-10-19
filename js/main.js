e4967b7673154029ea97552bc193cc5b6ece4528
crisgarm <crisgarm9@gmail.com>
2020-10-19 20:43:06 +0200

    Add css styles

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 function getRandomNumber(max) {
   const random = Math.ceil(Math.random() * max);
   return random;
- }
+}
 
 const input = document.querySelector(".js-input");
 const btn = document.querySelector(".js-button");
@@ -11,32 +11,28 @@ const clues = document.querySelector(".js-clues");
 const attempts = document.querySelector(".js-attempts");
 const random = getRandomNumber(100);
 console.log("Mi número aleatorio es " + random);
+let heGanado = false;
 
-function updateClick(){
+function updateClick() {
   const inputValue = parseInt(input.value);
   input.innerHTML = inputValue;
-  console.log(inputValue);
-  console.log(random);
   updateAttempts();
-  if (inputValue < 1 || inputValue > 100){
-    clues.innerHTML = "El número debe estar entre 1 y 100."
-    console.log(clues);
-   }else if (inputValue === random){
-    clues.innerHTML = "¡Has ganado campeona!"
-    console.log(clues);
-   }else if(inputValue > random){
-    clues.innerHTML = "Pista: Demasiado alto."
-    console.log(clues);
-    }else{
-    clues.innerHTML = "Pista: Demasiado bajo."
-    console.log(clues);
+  if (inputValue < 1 || inputValue > 100) {
+    clues.innerHTML = "El número debe estar entre 1 y 100.";
+  } else if (inputValue === random) {
+    heGanado = true;
+    clues.innerHTML = "¡Has ganado campeona!";
+  } else if (inputValue > random) {
+    clues.innerHTML = "Pista: Demasiado alto.";
+  } else {
+    clues.innerHTML = "Pista: Demasiado bajo.";
+  }
 }
-}
-
 let numberAttempts = 1;
 
-function updateAttempts(){
-  attempts.innerHTML = 'Número de intentos:' + numberAttempts++;
+function updateAttempts() {
+  if (heGanado == false) {
+    attempts.innerHTML = "Número de intentos:" + numberAttempts++;
+  }
 }
-
 btn.addEventListener("click", updateClick);
